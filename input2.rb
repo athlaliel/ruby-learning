4e780ab223b271fb287962cc6285613513f219f4
athlaliel <athla8452@gmail.com>
2020-08-18 17:34:25 +0900

    plus-qustion
    質問の追加。
    次は条件分岐と繰り返しを盛り込んでいく復習

--- input2.rb
+++ input2.rb
@@ -3,4 +3,21 @@ name = gets.chomp
 puts "次に好きな食べ物を教えて欲しいな"
 food = gets.chomp
 
-puts "あなたの名前は#{name}さんですね。\nそれから、好きな食べ物は#{food}ですね。\n教えてくれてどうもありがとう"
+puts "あなたの名前は#{name}さんですね。\nそれから、好きな食べ物は#{food}ですね。\n教えてくれてどうもありがとう\n次の質問です"
+
+brother_count = 0
+brothers = []
+
+puts "兄弟の名前は？"
+brothers << gets.chomp
+puts "兄弟は全部で何人ですか？"
+brothers << gets.to_i
+brother_count += 1
+
+puts "兄弟の名前は？"
+brothers << gets.chomp
+puts "兄弟は全部で何人ですか？"
+brothers << gets.to_i
+brother_count += 1
+
+puts "兄弟の人数は全部で#{brother_count}人です"
