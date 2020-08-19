e919c5153f01ee2b3407d1859991558747fbf238
athlaliel <athla8452@gmail.com>
2020-08-19 22:07:22 +0900

    if-times
    文法復習。これだけでも時間がかかってる。まだまだ理解が浅い

--- if.rb
+++ if.rb
@@ -1,9 +1,25 @@
-value = 3
+puts "次の質問に答えてください\n問題①\nあなたは男ですか？女ですか？"
+sex = gets.chomp
 
-if value > 0
-  puts "値は正です"
-elsif value < 0
-  puts "値は負です"
+if sex >= "男" && sex >= "女"
+  puts "あなたは男性ですね。ありがとう！"
+elsif sex == ""
+  puts "答えたくない.....そうですか....."
+elsif sex == "なし"
+  puts "無姓生命体なのですか？"
 else
-  puts "値は0です"
+  puts "あなたは女性ですね。ありがとう！"
+end
+
+puts "問題②\nあなたは日本人ですか？\nYES or NO を入力してください"
+japan = gets.chomp
+
+if japan >= "YES" && japan >= "NO"
+  puts "生粋の日本人ですね"
+elsif japan == ""
+  puts "えっ？答えたくないって？"
+else
+  puts "海外の方なんですね。日本へようこそ！"
+
+puts "以上であなたが#{sex}であること、加えて、日本人かの答えが#{japan}であることがわかりました。\nご協力どうもありがとう！"
 end
---
+++ times.rb
@@ -0,0 +1,3 @@
+5000000.times do |i|
+  puts i + 1
+end
