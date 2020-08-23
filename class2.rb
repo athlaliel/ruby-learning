cd4f1673d850f872829f5f7ebb940270ea923a7d
athlaliel <athla8452@gmail.com>
2020-08-23 18:20:00 +0900

    class2-update
    文法理解がまだまだ浅い。

--- class2.rb
+++ class2.rb
@@ -1,10 +1,25 @@
-class house
+class Smartphone
 
-  def maker(maker_number)
-    @maker_number = maker_number
+  def initialize(sumaho_name, sumaho_create, sumaho_memory)
+    @name = sumaho_name
+    @create = sumaho_create
+    @memory = sumaho_memory
   end
 
+  def self.sumaho_make
+    puts "該当メーカーのスマホを作成しました"
+  end
+
+  def sumaho_marker(sumaho_number)
+    @sumaho_number = sumaho_number
+    if sumaho_number > @memory
+      puts "#{@name}は、造っているメーカーが#{@create}なので、今はまだ#{@mamory}GB以上は販売していません"
+    else
+      puts "#{@memory}GB以下はもうキャリアショップでは入試できません"
+    end
+  end
 end
 
-maker = House.new() #インスタンスの生成と、変数定義
-maker.maker(4)
+sumaho = Smartphone.new("iPhone", "Apple", 512) #インスタンスと変数定義
+sumaho.sumaho_marker(2) #インスタンスの出力
+Smartphone.sumaho_make
