9fd737d64726d132d3b4315f69104cd0be4abd44
athlaliel <athla8452@gmail.com>
2020-08-24 13:33:31 +0900

    if-method

--- method.rb
+++ method.rb
@@ -7,3 +7,16 @@ puts "フルーツを入力してください"
 input = gets.chomp
 
 puts mixer(input)
+
+
+def study(programing, day)
+  puts "\n今日の学習内容は#{programing}ですね。\n今日もお疲れ様でした。"
+  return "#{programing}の学習は大変なので、毎日#{day}時間と目標を定めて継続学習していきましょう。"
+end
+
+puts "\n今日勉強した事を記入してください"
+type = gets.chomp
+puts "\n何時間勉強しましたか？"
+time = gets.to_i
+
+puts study(type, time)
