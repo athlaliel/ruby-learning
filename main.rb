7aeaa84fadbfef43ef990e3cfa3279dfe0375156
athlaliel <athla8452@gmail.com>
2020-08-31 16:29:33 +0900

    monster

--- main.rb
+++ main.rb
@@ -3,7 +3,6 @@ class Brave
   # attr_readerの記述でゲッターを省略することができる
   # 複数の値を同時に指定することができる
   attr_reader :name, :offense, :defense
-
   attr_accessor :hp
 
   # initializeメソッドを定義
@@ -16,19 +15,47 @@ class Brave
     @defense = params[:defense]
   end
 
+  def attack(monster)
+    puts "テスト:#{monster.name}のHPを表示 -> #{monster.hp}"
+  end
+
+end
+
+brave.attack(monster)
+
+class Monster
+
+  attr_reader :name, :offense, :defense
+  attr_accessor :hp
+
+  def initialize(**params)
+    @name = params[:name]
+    @hp = params[:hp]
+    @offense = params[:offense]
+    @defense = params[:defense]
+  end
+
 end
 
 brave = Brave.new(name:"テリー", hp:500,offense:150, defense:100)
+monster = Monster.new(name:"スライム", hp:250, offense:200, defense:100)
 
 # ヒアドキュメンと貴方で書けばputsや
 # クオーテーションを書く回数が減る。
-puts <<~TEXT
-NAME：#{brave.name}
-HP：#{brave.hp}
-OFFENSE：#{brave.offense}
-DEFENSE：#{brave.defense}
-TEXT
 
-brave.hp -= 30
+# puts <<~TEXT
+# NAME：#{brave.name}
+# HP：#{brave.hp}
+# OFFENSE：#{brave.offense}
+# DEFENSE：#{brave.defense}
+# TEXT
 
-puts "#{brave.name}はダメージを受けた！ 残りHPは#{brave.hp}だ"
+# brave.hp -= 30
+# puts "#{brave.name}はダメージを受けた！ 残りHPは#{brave.hp}だ"
+
+# puts <<~TEXT
+# NAME: #{monster.name}
+# HP: #{monster.hp}
+# OFFENSE: #{monster.offense}
+# DEFENSE: #{monster.defense}
+# TEXT
