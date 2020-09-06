2781a2b2850e4535a83b3b9923755aa2bc4d644f
athlaliel <athla8452@gmail.com>
2020-09-06 20:16:55 +0900

    brave-win-booleanType

--- games_controller.rb
+++ games_controller.rb
@@ -4,41 +4,51 @@ class Games_controller
   GOLD_CONSTANT = 3
 
   def battle(**params)
-    brave = params[:brave]
-    monster = params[:monster]
+    build_characters(params)
+    # brave = params[:brave]
+    # monster = params[:monster]
 
     loop do
-      brave.attack(monster)
-      break if battle_end?(monster)
-      monster.attack(brave)
-      break if battle_end?(brave)
+      @brave.attack(@monster)
+      break if battle_end?
+      @monster.attack(@brave)
+      break if battle_end?
     end
 
   # battle_result = brave.hp <= 0
-
-  if battle_result(brave)
-    result = calculate_of_exp_and_gold(monster)
-    puts "#{brave.name}は戦闘に勝利した！"
-    puts "#{result[:exp]}の経験値と#{result[:gold]}のお金をゲットした！"
-  else
-    puts "#{brave.name}は戦闘に敗北した......"
-    puts "GAME 0VER"
-  end
+  battle_judgement
 end
 
 private
 
-  def battle_end?(character)
-    character.hp <= 0
+  def build_characters(**params)
+    @brave = params[:brave]
+    @monster = params[:monster]
   end
 
-  def battle_result(brave)
-    brave.hp > 0
+  def battle_end?
+    @brave.hp <= 0 || @monster.hp <= 0
   end
 
-  def calculate_of_exp_and_gold(monster)
-    exp = (monster.offense + monster.defense)* EXP_CONSTANT
-    gold = (monster.offense + monster.defense)* GOLD_CONSTANT
+  def brave_win?
+    @brave.hp > 0
+  end
+
+  def battle_judgement
+
+    if brave_win?
+      result = calculate_of_exp_and_gold
+      puts "#{@brave.name}は戦闘に勝利した！"
+      puts "#{result[:exp]}の経験値と#{result[:gold]}のお金をゲットした！"
+    else
+      puts "#{@brave.name}は戦闘に敗北した......"
+      puts "GAME 0VER"
+    end
+  end
+
+  def calculate_of_exp_and_gold
+    exp = (@monster.offense + @monster.defense)* EXP_CONSTANT
+    gold = (@monster.offense + @monster.defense)* GOLD_CONSTANT
     result = {exp: exp, gold: gold}
 
     result
