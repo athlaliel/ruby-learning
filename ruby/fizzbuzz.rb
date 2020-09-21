f0948b9ff17a180126e74b1857338b5e39966f85
athlaliel <athla8452@gmail.com>
2020-09-21 22:20:55 +0900

    ruby_fizzbuzz

---
+++ ruby/fizzbuzz.rb
@@ -0,0 +1,17 @@
+(1..100).each do |n|
+  def fizzbuzz(n)
+    # if n % 15 == 0
+    if n % 3 == 0 && n % 5 == 0
+      puts "FizzBuzz"
+    elsif n % 3 == 0
+      puts "Fizz"
+    elsif n % 5 == 0
+      puts "Buzz"
+    else
+      puts n
+    end
+  end
+
+  # puts fizzbuzz(n)
+  fizzbuzz(n)
+end
