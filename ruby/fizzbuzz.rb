00ae2ce80c0311e8aeebc3a15f1412478b991c58
athlaliel <athla8452@gmail.com>
2020-10-27 19:00:21 +0900

    ruby_math_count

--- ruby/fizzbuzz.rb
+++ ruby/fizzbuzz.rb
@@ -28,9 +28,24 @@ end
     else
       puts n
     end
-    
-    fizzbuzz(n)
   end
+    fizzbuzz(n)
+end
+
+(1..100).each do |n|
+  def fizzbuzz(n) 
+    if n % 3 == 0 && n % 5 == 0
+      puts "fizzbuzz"
+    elsif n % 3 == 0
+      puts "fizz"
+    elsif n % 5 == 0
+      puts "buzz"
+    else
+      puts n
+    end
+  end
+
+  fizzbuzz(n)
 end
 
 
---
+++ ruby/math.rb
@@ -0,0 +1,2 @@
+n = gets.to_s.length
+puts n - 1
