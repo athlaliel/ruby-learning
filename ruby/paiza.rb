da6682cd605b87475f91aac8ce970161abf06e30
athlaliel <athla8452@gmail.com>
2020-11-04 19:05:50 +0900

    paiza_d_question

---
+++ ruby/paiza.rb
@@ -0,0 +1,11 @@
+# puts "何角形か算出せよ"
+# shape = gets.to_i / 180 + 2
+# puts shape
+
+puts "酒飲みと酒飲まない人の総額計算"
+alcoll = gets.to_i * 6000
+not_alcoll = gets.to_i * 4000
+puts alcoll + not_alcoll
+
+# alcoll = [gets.to_i * 6000, gets.to_i * 4000]
+# puts alcoll
