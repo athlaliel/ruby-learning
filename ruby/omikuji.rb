ba542e99361b17a2793493d26b713e39abe7d7e9
athlaliel <athla8452@gmail.com>
2020-11-13 23:20:56 +0900

    update_omikuji

--- ruby/omikuji.rb
+++ ruby/omikuji.rb
@@ -19,3 +19,18 @@ if omikuji > 30
 else 
   puts "大凶"
 end
+
+
+#下記のくじは失敗作、ランダムの比較演算子がうまく動いていない。
+# kuji = rand(1..50)
+# if kuji < rand(1..10)
+#     puts "番号は#{kuji}なので大吉です"
+# elsif kuji < rand(11..20)
+#     puts "番号は#{kuji}なので中吉です"
+# elsif kuji < rand(21..30)
+#     puts "番号は#{kuji}なので小吉です"
+# elsif kuji > rand(31..40)
+#     puts "番号は#{kuji}なので凶です"
+# else
+#     puts "番号は#{kuji}なので大凶です"
+# end
